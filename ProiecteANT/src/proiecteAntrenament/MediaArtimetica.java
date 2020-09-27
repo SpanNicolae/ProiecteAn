7abff836f9fab3a272decbf7c492d19738f42d63
Span Nicolae <nicolae.span@gmail.com>
2020-09-27 22:57:55 +0300

    Media Aritmetica

---
+++ ProiecteANT/src/proiecteAntrenament/MediaArtimetica.java
@@ -0,0 +1,19 @@
+package proiecteAntrenament;
+import java.util.*;
+public class MediaArtimetica {
+
+	public static void main(String[] args) {
+		System.out.println("Introduceti doua numere pozitive: ");
+		int b,c=0;
+		for(int i = 1; i<3;i++) {
+			Scanner a = new Scanner(System.in);
+			b = a.nextInt();
+			c=c+b;
+			
+		}
+		c=c/2;
+		System.out.println("Media aritmetica a numerelor introduse este: "+c);
+
+	}
+
+}
